fb922d34cc1dc82885e866d5c2aa0a5fd12d5d95
William Moraes <wwmoraes@users.noreply.github.com>
2017-04-03 16:57:15 -0300

    Changed version number

--- Native.js
+++ Native.js
@@ -1,7 +1,7 @@
 // @name         Native
 // @description  Save native functions to allow overrides to fix/extend functionality.
 // @author       2013, William Moraes (http://wwmoraes.com/)
-// @version      0.1
+// @version      1.0
 // @copyright    2013, William Moraes (http://wwmoraes.com/)
 // @license      GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @homepageURL  https://github.com/wwmoraes/userscripts
