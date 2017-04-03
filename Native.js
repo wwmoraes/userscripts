ad3f110e7cb7c494c907850c585a0c04a7db18a8
William Moraes <wwmoraes@users.noreply.github.com>
2017-04-03 13:17:00 -0300

    Removed userscript tags to act as a library

--- Native.js
+++ Native.js
@@ -1,23 +1,26 @@
-// ==UserScript==
 // @name         Native Overrides
-// @namespace    http://wwmoraes.com/
+// @description  Save native functions to allow overrides to fix/extend functionality.
+// @author       2013, William Moraes (http://wwmoraes.com/)
 // @version      0.1
-// @description  Save native functions and overrides them to fix/extend functionality.
-// @author       William Moraes
 // @match        *://*/*
 // @grant        none
-// ==/UserScript==
+// @copyright    2013, William Moraes (http://wwmoraes.com/)
+// @license      GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @homepageURL  https://github.com/wwmoraes/userscripts
+// @supportURL   https://github.com/wwmoraes/userscripts/issues
+// @updateURL    https://openuserjs.org/meta/wwmoraes/Native.meta.js
+// @downloadURL  https://openuserjs.org/src/scripts/wwmoraes/Native.js
 
 (function() {
     'use strict';
 
     // Native saves
-    var Native = {
+    window.Native = {
         window: {
             open: window.open
         }
     };
 
-    // Override to prevent popups (ignores third parameter)
-    window.open = function(url, target){Native.window.open.apply(this, Array.prototype.slice.call(arguments, 0, 2));};
+    // Example: Override to prevent popups (ignores third parameter)
+    //window.open = function(url, target){Native.window.open.apply(this, Array.prototype.slice.call(arguments, 0, 2));};
 })();
