13bbe907cefe619e6df6c98d9e2a654a4600b292
William Moraes <wwmoraes@users.noreply.github.com>
2017-04-03 17:31:39 -0300

    Added regex to prevent harmful code eval
    This matches only objects and methods, dropping any possible call.

--- Native.js
+++ Native.js
@@ -1,7 +1,7 @@
 // @name         Native
 // @description  Save native functions to allow overrides to fix/extend functionality.
 // @author       2013, William Moraes (http://wwmoraes.com/)
-// @version      1.0
+// @version      1.1
 // @copyright    2013, William Moraes (http://wwmoraes.com/)
 // @license      GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @homepageURL  https://github.com/wwmoraes/userscripts
@@ -23,6 +23,7 @@
 	Function.override = function(oldFunctionName, newFunction){
 		if(typeof(oldFunctionName) !== "string") throw new TypeError('oldFunctionName must be a string');
 		if(typeof(newFunction) !== "function") throw new TypeError('newFunction must be a function');
+		oldFunctionName = oldFunctionName.match(/[A-Za-z0-9._$]+/g)[0];
 		var nativeName = "Object.Natives['"+oldFunctionName+"']";
 		var nativeFuncType = typeof(eval(nativeName));
 		if(oldFunctionName != "" && nativeFuncType === "undefined")
@@ -33,6 +34,7 @@
 	
 	Function.restore = function(oldFunctionName){
 		if(typeof(oldFunctionName) !== "string") throw new TypeError('oldFunctionName must be a string');
+		oldFunctionName = oldFunctionName.match(/[A-Za-z0-9._$]+/g)[0];
 		var nativeName = "Object.Natives['"+oldFunctionName+"']";
 		var nativeFuncType = typeof(eval(nativeName));
 		if(oldFunctionName != "" && nativeFuncType !== "undefined")
