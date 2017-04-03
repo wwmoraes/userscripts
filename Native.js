ebd0a4644dc377e5d566412120b9169df60869c9
William Moraes <wwmoraes@users.noreply.github.com>
2017-04-03 13:27:34 -0300

    renamed and removed unneeded tags

--- Native.js
+++ Native.js
@@ -1,15 +1,11 @@
-// @name         Native Overrides
+// @name         Native
 // @description  Save native functions to allow overrides to fix/extend functionality.
 // @author       2013, William Moraes (http://wwmoraes.com/)
 // @version      0.1
-// @match        *://*/*
-// @grant        none
 // @copyright    2013, William Moraes (http://wwmoraes.com/)
 // @license      GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @homepageURL  https://github.com/wwmoraes/userscripts
 // @supportURL   https://github.com/wwmoraes/userscripts/issues
-// @updateURL    https://openuserjs.org/meta/wwmoraes/Native.meta.js
-// @downloadURL  https://openuserjs.org/src/scripts/wwmoraes/Native.js
 
 (function() {
     'use strict';
