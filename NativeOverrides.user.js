47d96a4f4e6b452c9e700eb29a22faa0489954dc
Martii <mmartz@users.sourceforge.net>
2017-11-28 23:26:58 -0700

    Fixes for OUJS
    * Use SPDX codes for GPL-3.0
    * Add a missing copyright and set to year that used to be in `@author`
    
    Without these you will be unable to modify these scripts already posted on OUJS.
    
    Thanks.

--- NativeOverrides.user.js
+++ NativeOverrides.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Native Overrides
 // @description  Overrides native functions using Native library to fix/extend functionality.
-// @author       2013, William Moraes (http://wwmoraes.com/)
+// @author       William Moraes (http://wwmoraes.com/)
 // @version      0.2
 // @match        *://*/*
 // @grant        none
 // @require      https://openuserjs.org/src/libs/wwmoraes/Native.js
 // @copyright    2013, William Moraes (http://wwmoraes.com/)
-// @license      GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @license      GPL-3.0; http://www.gnu.org/copyleft/gpl.html
 // @homepageURL  https://github.com/wwmoraes/userscripts
 // @supportURL   https://github.com/wwmoraes/userscripts/issues
 // @updateURL    https://openuserjs.org/meta/wwmoraes/Native.meta.js
@@ -16,11 +16,11 @@
 
 (function(){
 	'use strict';
-	
+
 	document.addEventListener("pageshow", function(){
 		Function.override('window.open', function(){base.apply(this, Array.prototype.slice.call(arguments, 0, 2));});
 	});
-	
+
 	document.addEventListener("pagehide", function(){
 		Function.restore('window.open');
 	});
